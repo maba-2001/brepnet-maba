5a43507706feb66cafcbdd1e9c8df2c14a88745e
matteoballegeer <matteo.ballegeer@ugent.be>
2025-02-04 20:44:48 +0100

    Initial commit, restructuring and packaging

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='brepnet_maba',
+    version='1.0.0',
+    description='The BrepNet code, modified for our use case',
+    url = 'https://github.com/maba-2001/brepnet-maba',
+    packages=['brepnet_maba']
+)
